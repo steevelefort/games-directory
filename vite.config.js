97edfcd44af1ff8d506f0873978d99c1cc9952e2
Steeve Lefort <steeve@2a01cb012045c45ca94477a46d67143c.ipv6.abo.wanadoo.fr>
2025-02-17 12:21:59 +0100

    change start_url

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
         short_name: 'Jeux',                     // Nom court pour l'écran d'accueil
         description: 'Application de recherche de jeux vidéo',
         theme_color: '#ffffff',                 // Couleur du thème
-        start_url: '/',                         // Page de démarrage
+        start_url: 'https://steevelefort.github.io/games-directory/',                         // Page de démarrage
         display: 'standalone',                  // Mode d'affichage
         background_color: '#ffffff',            // Couleur de fond au démarrage
 
